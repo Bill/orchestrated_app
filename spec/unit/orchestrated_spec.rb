5463ed34dce0a3013cfc01992fcadcf27a711d28
Bill <bill.burcham@gmail.com>
2012-12-31 11:52:22 -0800

    removed redundant specs

--- spec/unit/orchestrated_spec.rb
+++ spec/unit/orchestrated_spec.rb
@@ -33,35 +33,6 @@ describe Orchestrated do
       it 'should return an Orchestration object' do
         expect(@result).to be_kind_of(Orchestrated::CompletionExpression)
       end
-      it 'should enqueue a job' do
-        expect(DJ.job_count).to eq(1)
-      end
-      context 'after work_off' do
-        it 'should invoke the orchestrated method' do
-          First.any_instance.should_receive(:do_first_thing)
-          DJ.work
-        end
-        it 'should pass a parameter to the orchestrated object' do
-          First.any_instance.should_receive(:do_first_thing).with(2)
-          DJ.work
-        end
-      end
-    end
-    context 'orchestrating with a simple prerequisite' do
-      let(:s) {Second.new}
-      before(:each){@result = s.orchestrated( f.orchestrated.do_first_thing(2)).do_second_thing(3)} # 3 is a prime number
-      context 'after completing the prerequisite' do
-        context 'next work_off' do
-          it 'should invoke the orchestrated method' do
-            Second.any_instance.should_receive(:do_second_thing).exactly(1).times
-            DJ.work(2)
-          end
-          it 'should pass a parameter to the orchestrated object' do
-            Second.any_instance.should_receive(:do_second_thing).with(3)
-            DJ.work(2)
-          end
-        end
-      end
     end
   end
 end
